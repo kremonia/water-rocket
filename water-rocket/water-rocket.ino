7240508796603637efb1c77de52a49c080949400
Alexander Cheorny <kremonia@gmail.com>
2019-06-09 19:18:13 +0300

    Added Visual Studio Project

---
+++ water-rocket/water-rocket.ino
@@ -0,0 +1,18 @@
+/*
+ Name:		water-rocket.ino
+ Created:	6/9/2019 6:08:29 PM
+ Author:	Alexander Cheorny
+*/
+
+#include <Adafruit_BMP280.h>
+#include <EEPROM.h>
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+	
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
